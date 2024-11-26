cc274245b0276ee4f6fa476299af3a32ed71f879
yashje <yash959973@gmail.com>
2024-11-26 17:42:21 +0530

    change in MainNav component

--- src/components/MainNav.tsx
+++ src/components/MainNav.tsx
@@ -1,6 +1,6 @@
 import { useAuth0 } from "@auth0/auth0-react"
 import { Button } from "./ui/button"
-import UserNameMenu from "./userNameMenu"
+import UserNameMenu from "./UserNameMenu.tsx"
 
 const MainNav = () => {
 
