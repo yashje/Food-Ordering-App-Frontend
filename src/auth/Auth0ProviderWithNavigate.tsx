b3f5151c27e3a1caf7bd6de603faa16fb852d779
yashje <yash959973@gmail.com>
2024-11-26 17:33:22 +0530

    Change in Auth0ProviderWithNavigate

--- src/auth/Auth0ProviderWithNavigate.tsx
+++ src/auth/Auth0ProviderWithNavigate.tsx
@@ -1,4 +1,4 @@
-import { AppState, Auth0Provider, User } from "@auth0/auth0-react";
+import {  Auth0Provider } from "@auth0/auth0-react";
 import { useNavigate } from "react-router-dom";
 
 type Props = {
@@ -18,7 +18,7 @@ const Auth0ProviderWithNavigate: React.FC<Props> = ({ children }): JSX.Element =
         throw new Error(`unable to initialise auth ${domain} ${clientId} ${redirect_uri}`)
     }
 
-    const onRedirectCallback = (appState?: AppState, user?: User) => {
+    const onRedirectCallback = () => {
         navigate("/auth-callback")
     }
 
