7c8ba83b160f0772344685ef34d2286cc92058aa
yashje <yash959973@gmail.com>
2024-12-08 06:44:33 +0530

    search feature

--- src/AppRoutes.tsx
+++ src/AppRoutes.tsx
@@ -5,20 +5,50 @@ import AuthCallbackPage from "./pages/AuthCallbackPage.tsx";
 import UserProfilePage from "./pages/UserProfilePage.tsx";
 import ProtectedRoute from "./auth/ProtectedRoute.tsx";
 import ManageRestaurantPage from "./pages/ManageRestaurantPage.tsx";
+import SearchPage from "./pages/SearchPage.tsx";
 
 
 const AppRoutes = () => {
     return (
-        <Routes>
-            <Route path="/" element={<Layouts showHero={true}><HomePage /></Layouts>} />
-            <Route path="/auth-callback" element={<AuthCallbackPage />} />
-            <Route element={<ProtectedRoute />} >
-                <Route path="/user-profile" element={<Layouts><UserProfilePage /></Layouts>} />
-                <Route path="/manage-restaurant" element={<Layouts><ManageRestaurantPage /></Layouts> }/>
-            </Route>
-            <Route path="*" element={<Navigate to="/" />} />
-        </Routes>
-    )
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <Layouts showHero={true}>
+              <HomePage />
+            </Layouts>
+          }
+        />
+        <Route path="/auth-callback" element={<AuthCallbackPage />} />
+        <Route
+          path="/search/:city"
+          element={
+            <Layouts showHero={false}>
+              <SearchPage />
+            </Layouts>
+          }
+        />
+        <Route element={<ProtectedRoute />}>
+          <Route
+            path="/user-profile"
+            element={
+              <Layouts>
+                <UserProfilePage />
+              </Layouts>
+            }
+          />
+          <Route
+            path="/manage-restaurant"
+            element={
+              <Layouts>
+                <ManageRestaurantPage />
+              </Layouts>
+            }
+          />
+        </Route>
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
+    );
 }
 
 export default AppRoutes
